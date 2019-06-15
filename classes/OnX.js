1e8dae600102946d9c7eba7ab31f67933fe28b2f
Karl Neugebauer <git@karlneugebauer.com>
2019-06-15 11:57:14 +0200

    automaticall split long lines

--- classes/OnX.js
+++ classes/OnX.js
@@ -23,8 +23,10 @@ module.exports = class OnX {
   async function onChat (msg) {
     Logger.info("--> " + msg.channel + " " + msg.username + ": " + msg.message)
 
+    msg.message += " "
+
     /* update the bot */
-    if (msg.message.startsWith("<update") && msg.tags.userId === "38949074") {
+    if (msg.message.startsWith("<update ") && msg.tags.userId === "38949074") {
       let { spawn } = require( 'child_process' ); let ls = spawn( 'git', [ 'pull' ] )
 
       ls.on( 'close', code => {
@@ -35,18 +37,22 @@ module.exports = class OnX {
     }
 
     /* Shutting down the bot */
-    if (msg.message.startsWith("<s") && msg.tags.userId === "38949074") {
+    if (msg.message.startsWith("<s ") && msg.tags.userId === "38949074") {
       this.say(msg.channel, "Shutting down FeelsBadMan")
       setTimeout(function () {
         process.exit(0)
       }, 1200)
     }
 
-    if (msg.message.startsWith("<tags")) {
+    if (msg.message.startsWith("<tags ")) {
       this.queue.sayWithBoth(msg.tags.roomId, msg.channel, JSON.stringify(msg.tags, null, 2), msg.tags.userId)
     }
 
-    if (msg.message.startsWith("<")) {
+    if (msg.message.startsWith("<y ")) {
+      this.queue.sayWithBoth(msg.tags.roomId, msg.channel, "word1{nl}word2 {nl}word3{nl} word4 {nl} word5 {nl} {nl}", msg.tags.userId)
+    }
+
+    if (msg.message.startsWith("< ")) {
       this.queue.sayWithBoth(msg.tags.roomId, msg.channel, ">", msg.tags.userId)
       this.queue.sayWithBoth(msg.tags.roomId, msg.channel, ">", msg.tags.userId)
       this.queue.sayWithBoth(msg.tags.roomId, msg.channel, ">------------", msg.tags.userId)
--- classes/Queue.js
+++ classes/Queue.js
@@ -6,7 +6,11 @@ const BasicBucket = require('../classes/BasicBucket.js')
 const UserIdLoginCache = require('../classes/UserIdLoginCache.js')
 //ENUMS
 const UserLevels = require('../ENUMS/UserLevels.js')
+//other consts
 const TIMEOUT_OFFSET = 100 //ms
+const MAX_MESSAGE_LENGTH = 500 * 0.9
+const MIN_MESSAGE_CUT_LENGTH = MAX_MESSAGE_LENGTH * 0.75
+const NEWLINE_SEPERATOR = "{nl}"
 
 
 module.exports = class Queue {
@@ -41,88 +45,92 @@ module.exports = class Queue {
     //if userId paramter is missing just set it to "-1"
     userId = userId || "-1"
 
-    /* TODO: if message is longer than 500 chars split it smartly */
+    message = this.splitRecursively(message)
+
     /* TODO: newLine stuff */
 
     this.messageQueue.push({checked: false, isBeingChecked: false, channelId, channelName, message, userId})
     this.queueEmitter.emit('event')
   }
 
+  /**
+   * Recursively splits a message based on MAX_MESSAGE_LENGTH and MIN_MESSAGE_CUT_LENGTH.
+   * Inserts NEWLINE_SEPERATOR into the gap
+   * @param message inputmessage
+   * @returns {string} split message
+   */
+  splitRecursively (message) {
+    if (message.length > MAX_MESSAGE_LENGTH) {
+      let indexOfLastSpace = message.substring(0, MAX_MESSAGE_LENGTH).lastIndexOf(' ')
+      if (indexOfLastSpace < MIN_MESSAGE_CUT_LENGTH) {
+        indexOfLastSpace = MAX_MESSAGE_LENGTH
+      }
+      return message.substring(0, indexOfLastSpace).trim()
+              + NEWLINE_SEPERATOR
+              + this.splitRecursively(message.substring(indexOfLastSpace).trim())
+    }
+    return message
+  }
+
   async checkQueue () {
-    if (this.messageQueue.length > 0) {
-      let msgObj = this.messageQueue[0]
-      if (!msgObj.isBeingChecked) {
-        msgObj.isBeingChecked = true
-
-        let channel = this.bot.chat.channels[msgObj.channelId]
-        let botStatus = channel.botStatus
-
-        /**
-         * there is a tiny chance that botStatus is undefined ... if so check again in 5 ms
-         * same with channel.lastMessage and channel.lastMessageTimeMillis
-         * The reason for that is that when they get updated there are undefined for a a millisecond or two
-         */
-        if (typeof botStatus === 'undefined' || botStatus === null
-          || !channel.hasOwnProperty('lastMessage') || !channel.hasOwnProperty('lastMessageTimeMillis')
-        ) {
-          await sleep(5)
-          msgObj.isBeingChecked = false
-          this.queueEmitter.emit('event')
-          return
-        }
-
-        /* Is timeout over? (sending message too fast) */
-        let currentTimeMillis = Date.now()
-        if (botStatus < UserLevels.VIP && currentTimeMillis < channel.lastMessageTimeMillis + 1000 + TIMEOUT_OFFSET) {
-          await sleep( channel.lastMessageTimeMillis - currentTimeMillis + 1000 + TIMEOUT_OFFSET)
-          msgObj.isBeingChecked = false
-          this.queueEmitter.emit('event')
-          return
-        }
-        channel.lastMessageTimeMillis = currentTimeMillis
-
-        /* User ticket */
-        if (botStatus < UserLevels.VIP) {
-          if (!this.privsgUserBucket.takeTicket()) {
-            Logger.info("Denied user ticket")
-            await sleep(1500)
-            msgObj.isBeingChecked = false
-            this.queueEmitter.emit('event')
-            return
-          }
-        }
-        /* Moderator ticket */
-        if (!this.privmsgModeratorbucket.takeTicket()) {
-          Logger.info("Denied moderator ticket")
-          await sleep(1500)
-          msgObj.isBeingChecked = false
-          this.queueEmitter.emit('event')
-          return
-        }
-
-        /*TODO:  is message safe to post --- else: this.messageQueue.shift() */
-
-        /* Duplicate message */
-        if (msgObj.message === channel.lastMessage) {
-          msgObj.message += " \u{E0000}"
-        }
-        channel.lastMessage = msgObj.message
-
-        /* actual saying */
-        this.bot.chat.say(msgObj.channelName, msgObj.message).then(() => {
-
-          this.messageQueue.shift()
-          Logger.info("<-- " + msgObj.channelName + " " + this.bot.userName + ": " + msgObj.message)
-          this.queueEmitter.emit('event')
-        }).catch(async () => {
-          Logger.info("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
-          Logger.info("Dropped: " + msgObj.message)
-          await sleep(5000)
-          msgObj.isBeingChecked = false
-          this.queueEmitter.emit('event')
-        })
+    if (this.messageQueue.length <= 0) {
+      return
+    }
+    let msgObj = this.messageQueue[0]
+    if (msgObj.isBeingChecked) {
+      return
+    }
+    msgObj.isBeingChecked = true
+    let channel = this.bot.chat.channels[msgObj.channelId]
+    let botStatus = channel.botStatus
+    if (typeof botStatus === 'undefined' || botStatus === null
+      || !channel.hasOwnProperty('lastMessage') || !channel.hasOwnProperty('lastMessageTimeMillis')
+    ) {
+      await sleep(5)
+      msgObj.isBeingChecked = false
+      this.queueEmitter.emit('event')
+      return
+    }
+    let currentTimeMillis = Date.now()
+    if (botStatus < UserLevels.VIP && currentTimeMillis < channel.lastMessageTimeMillis + 1000 + TIMEOUT_OFFSET) {
+      await sleep(channel.lastMessageTimeMillis - currentTimeMillis + 1000 + TIMEOUT_OFFSET)
+      msgObj.isBeingChecked = false
+      this.queueEmitter.emit('event')
+      return
+    }
+    channel.lastMessageTimeMillis = currentTimeMillis
+    if (botStatus < UserLevels.VIP) {
+      if (!this.privsgUserBucket.takeTicket()) {
+        Logger.info("Denied user ticket")
+        await sleep(1500)
+        msgObj.isBeingChecked = false
+        this.queueEmitter.emit('event')
+        return
       }
     }
+    if (!this.privmsgModeratorbucket.takeTicket()) {
+      Logger.info("Denied moderator ticket")
+      await sleep(1500)
+      msgObj.isBeingChecked = false
+      this.queueEmitter.emit('event')
+      return
+    }
+    if (msgObj.message === channel.lastMessage) {
+      msgObj.message += " \u{E0000}"
+    }
+    channel.lastMessage = msgObj.message
+    this.bot.chat.say(msgObj.channelName, msgObj.message).then(() => {
+
+      this.messageQueue.shift()
+      Logger.info("<-- " + msgObj.channelName + " " + this.bot.userName + ": " + msgObj.message)
+      this.queueEmitter.emit('event')
+    }).catch(async () => {
+      Logger.info("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+      Logger.info("Dropped: " + msgObj.message)
+      await sleep(5000)
+      msgObj.isBeingChecked = false
+      this.queueEmitter.emit('event')
+    })
   }
 }
 
