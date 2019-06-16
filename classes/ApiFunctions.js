f8fe799e55eae0b001fb690e9f8a4f30530dc692
Karl Neugebauer <git@karlneugebauer.com>
2019-06-16 10:40:17 +0200

    updated ApiFunctions to use bare nodejs https.request

--- classes/ApiFunctions.js
+++ classes/ApiFunctions.js
@@ -1,38 +1,134 @@
 "use strict"
 const Logger = require('consola')
+const https = require('https')
 //ENUMS
 const UserLevels = require('../ENUMS/UserLevels.js')
 const ChatLimit = require('../ENUMS/ChatLimit.js')
 
 const UPDATE_BOT_STATUS_INTERVAL = 15000 //ms
 
+const TWITCH_API_KRAKEN = {
+  host: "api.twitch.tv",
+  path: "/kraken/",
+  method: 'GET',
+  headers: {
+    'Accept': 'application/vnd.twitchtv.v5+json',
+  }
+}
+
+const TWITCH_API_HELIX = {
+  host: "api.twitch.tv",
+  path: "/helix/",
+  method: 'GET',
+  headers: {}
+}
+
 
 module.exports = class ApiFunctions {
   constructor (bot) {
     this.bot = bot
+    TWITCH_API_KRAKEN.headers["Client-ID"] = bot.chat.botData.clientID
+    TWITCH_API_HELIX.headers["Client-ID"] = bot.chat.botData.clientID
 
     setInterval(this.updateBotStatus.bind(this), UPDATE_BOT_STATUS_INTERVAL)
   }
 
   /**
-   * Return the UserID from a username
-   * Example: https://api.twitch.tv/kraken/users?api_version=5&login=icdb
-   * @param  {String} username The name to check for
-   * @return {String}        userID or -1 when no user found
+   * Creates an API request towards twitch API version 5 (kraken)
+   * It's recommended to use await to call this function
+   * @param pathAppend path after *api.twitch.tv/kraken/'
+   * @returns {Promise<object>} return from the API parsed from string to json
    */
-  async userIdFromLogin (username) {
-    username = username.replace(/#/, '')
+  async apiRequestKraken (pathAppend) {
+    return await this.apiRequestJson(TWITCH_API_KRAKEN, pathAppend)
+  }
+
+  /**
+   * Creates an API request towards twitch API version ? (helix)
+   * It's recommended to use await to call this function
+   * @param pathAppend path after *api.twitch.tv/helix/'
+   * @returns {Promise<object>} return from the API parsed from string to json
+   */
+  async apiRequestHelix (pathAppend) {
+    return await this.apiRequestJson(TWITCH_API_HELIX, pathAppend)
+  }
+
+  /**
+   * Create an https request
+   * @param requestBase Base object to clone to use for the request
+   * @param pathAppend path appended to requestBase.path
+   * @returns {Promise<*>} return body parsed to json
+   */
+  async apiRequestJson (requestBase, pathAppend) {
+    return JSON.parse(await this.apiRequestString(requestBase, pathAppend))
+  }
+
+  /**
+   * Create an https request
+   * @param requestBase Base object to clone to use for the request
+   * @param pathAppend path appended to requestBase.path
+   * @returns {Promise<*>} return body as string
+   */
+  async apiRequestString (requestBase, pathAppend) {
     return new Promise((resolve, reject) => {
-      this.bot.api.get('users', {'version': 'kraken', search: {'api_version': '5', 'client_id': this.bot.chat.botData.clientID, 'login': username}}).then(response => {
-        if (response.total === 0) {
-          resolve("-1")
-        } else {
-          resolve(response.users[0].id)
-        }
-      }).catch((err) => {
+      //Duplicate default request object
+      let request = Object.assign({}, requestBase)
+      request.path += pathAppend
+
+      let req = https.request(request, (res) => {
+        res.setEncoding('utf8')
+        res.on('data', (response) => {
+          resolve(response)
+        })
+      })
+      req.on('error', (err) => {
         reject(err)
       })
+      req.write('')
+      req.end()
     })
+  }
+
+  /**
+   * receive login name from a single userid
+   * @param userId userid to check
+   * @returns {Promise<string>} login name as string inside a promise
+   */
+  async loginFromUserId (userId) {
+    let response = await this.userInfo(userId)
+    if (response.hasOwnProperty('_id')) {
+      return response['_id']
+    } else {
+      return ""
+    }
+  }
+
+  /**
+   * Returns the userId from a single login
+   * @param username login to check
+   * @returns {Promise<string>} userId as string
+   */
+  async userIdFromLogin (username) {
+    let response = this.userInfosFromLogins([username])
+
+    if (response.total === 0) {
+      return '-1'
+    } else {
+      return response.users[0].id
+    }
+  }
+
+  /**
+   * Return the userInfo from an array of usernames
+   *
+   * @param  {Array<String>} usernames The names to check for
+   * @return {Object} return from users api
+   */
+  async userInfosFromLogins (usernames) {
+    usernames.map((entry) => {
+      return entry.replace(/#/, '')
+    })
+    return await this.apiRequestKraken('users?login=' + usernames.join(','))
   }
 
   /**
@@ -67,15 +163,8 @@ module.exports = class ApiFunctions {
    * @return {Users} [description]
    */
   async userInfo (userId) {
-    return new Promise((resolve, reject) => {
-      this.bot.api.get('users/' + userId + '/chat', {'version': 'kraken', search: {'api_version': '5', 'client_id': this.bot.chat.botData.clientID}}).then(response => {
-        resolve(response)
-      }).catch((err) => {
-        reject(err)
-      })
-    })
+    return await this.apiRequestKraken('users/' + userId + '/chat')
   }
-
 
   /**
    * Accesses the kraken/users/:userID/chat/channels/:roomID
@@ -100,13 +189,7 @@ module.exports = class ApiFunctions {
    * @return {Users} [description]
    */
   async userInChannelInfo (userId, roomId) {
-    return new Promise((resolve, reject) => {
-      this.bot.api.get('users/' + userId + '/chat/channels/' + roomId, {'version': 'kraken', search: {'api_version': '5', 'client_id': this.bot.chat.botData.clientID}}).then(response => {
-        resolve(response)
-      }).catch((err) => {
-        reject(err)
-      })
-    })
+    return await this.apiRequestKraken('users/' + userId + '/chat/channels/' + roomId)
   }
 
   /**
