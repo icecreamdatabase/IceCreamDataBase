698b4ee50e4c77d4c1c52625f00acc254118e395
Karl <git@karlneugebauer.com>
2019-07-17 15:28:09 +0200

    disabled some debug and hopfully fixed some stuff

--- modules/UserNotice.js
+++ modules/UserNotice.js
@@ -44,7 +44,7 @@ module.exports = class UserNotice {
               this.bot.chat.queue.sayWithBoth(msg.tags.roomId, msg.channel, announcementMessage, msg.tags.userId)
             }
           } else {
-            DiscordLog.debug(__filename + ": No data for " + msg.tags.roomId + " in notificationData")
+            //DiscordLog.debug(__filename + ": No data for " + msg.tags.roomId + " in notificationData")
           }
         } else {
           DiscordLog.error(__filename + ": No roomId in msg.tags")
