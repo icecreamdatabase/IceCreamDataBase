4906194807b35f5b24f71ba38c210a886ef5b455
cineafx <git@karlneugebauer.com>
2019-08-22 15:34:33 +0200

    put the pipe into one line

--- classes/modules/PrivMsg.js
+++ classes/modules/PrivMsg.js
@@ -56,8 +56,7 @@ module.exports = class PrivMsg {
       let badges = messageObj.raw.tags.badges
       if (badges !== true) {
         let badgeSplit = badges.split(",")
-        badgeSplit = badgeSplit.map((x) => UserLevels[x.split("/")[0].toUpperCase()])
-        badgeSplit = badgeSplit.filter(Boolean)
+        badgeSplit = badgeSplit.map((x) => UserLevels[x.split("/")[0].toUpperCase()]).filter(Boolean)
         messageObj.userLevel = Math.max(...badgeSplit)
       }
     }
