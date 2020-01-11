5a6f7ed4e33f52c68f08996f1da4fcf3354890b1
Karl <git@karlneugebauer.com>
2020-01-11 21:03:14 +0100

    added interactive parameter

---
+++ website/tts/all/tts.js
@@ -0,0 +1,94 @@
+//default parameter
+const voices = ["Aditi", "Amy", "Astrid", "Bianca", "Brian", "Carla", "Carmen", "Celine", "Chantal", "Conchita", "Cristiano", "Dora", "Emma", "Enrique", "Ewa", "Filiz", "Geraint", "Giorgio", "Gwyneth", "Hans", "Ines", "Ivy", "Jacek", "Jan", "Joanna", "Joey", "Justin", "Karl", "Kendra", "Kimberly", "Liv", "Lotte", "Mads", "Maja", "Marlene", "Mathieu", "Matthew", "Maxim", "Mia", "Miguel", "Mizuki", "Naja", "Nicole", "Penelope", "Raveena", "Ricardo", "Ruben", "Russell", "Salli", "Seoyeon", "Takumi", "Tatyana", "Vicki", "Vitoria", "Zhiyu", /* Less refinded ones */ "An", "Andika", "Asaf", "Danny", "Filip", "Guillaume", "HanHan", "Heather", "Heidi", "Hemant", "Herena", "Hoda", "Huihui", "Ivan", "Jakub", "Kalpana", "Kangkang", "Karsten", "Lado", "Linda", "Matej", "Michael", "Naayf", "Pattara", "Rizwan", "Sean", "Stefanos", "Szabolcs", "Tracy", "Valluvar", "Yaoyao", "Zhiwei"]
+const defaultVoice = "Brian"
+const defaultTtsRateLimit = 1000
+
+
+//get parameters out of url
+let channels = (findGetParameter("channels") || "").split(",").filter(Boolean)
+let singleChannel = findGetParameter("channel")
+if (singleChannel) {
+  channels.push(singleChannel)
+}
+let sayNames = !!findGetParameter("names")
+let voice = findGetParameter("voice") || defaultVoice
+let ttsRatelimit = parseInt(findGetParameter("ttsRateLimit")) || defaultTtsRateLimit
+let noSkip = !!findGetParameter("noSkip")
+let volume = parseFloat(findGetParameter("volume") || "1")
+let interactive = !!findGetParameter("interactive")
+
+//needed variables
+let shouldSend = true
+let audioPlaying = false
+
+//apply values
+document.getElementById("player").volume = volume
+document.getElementById("interactive").style.display = interactive ? "block" : "none"
+
+//Eventlistener for TTS audio done playing
+document.getElementById("player").addEventListener("ended", () => audioPlaying = false)
+
+
+/* twitch-js part */
+// noinspection JSUnresolvedFunction
+const {api, chat} = new TwitchJs({token: "randomstring", username: "justinfan47", log: {level: "warn"}})
+
+chat.connect().then(() => {
+  channels.forEach(channel => chat.join(channel))
+})
+
+chat.on("PRIVMSG", (msgObj) => {
+  if (shouldSend && (!interactive || document.getElementById("checkbox-tts-enabled").checked)) {
+    shouldSend = false
+    let ttsMessage = msgObj.message
+    if (sayNames) {
+      ttsMessage = msgObj.username + " says " + ttsMessage
+    }
+    console.log("TTS: " + msgObj.username + ": " + msgObj.message)
+    setTimeout(() => shouldSend = true, ttsRatelimit)
+    speak(ttsMessage, voice)
+  }
+})
+
+async function speak(text, voice = "Brian") {
+  if (noSkip && audioPlaying) {
+    return
+  }
+  audioPlaying = true
+  voice = voices.find(x => x.toLowerCase() === voice.toLowerCase()) || defaultVoice
+  let speak = await fetch("https://api.streamelements.com/kappa/v2/speech?voice=" +
+    voice +
+    "&text=" +
+    encodeURIComponent(text.trim()))
+  if (speak.status !== 200) {
+    console.warn(await speak.text())
+    return
+  }
+  let mp3 = await speak.blob()
+  let blobUrl = URL.createObjectURL(mp3)
+  document.getElementById("source").setAttribute("src", blobUrl)
+  let player = document.getElementById("player")
+  player.pause()
+  player.load()
+  player.play()
+}
+
+function findGetParameter(parameterName) {
+  let result = null
+  let tmp = []
+  location.search
+    .substr(1)
+    .split("&")
+    .forEach(function (item) {
+      tmp = item.split("=")
+      if (tmp[0].toLowerCase() === parameterName.toLowerCase()) {
+        result = decodeURIComponent(tmp[1])
+      }
+    })
+  return result
+}
+
+function onVolumechange (value) {
+  document.getElementById("player").volume = value
+}
+
