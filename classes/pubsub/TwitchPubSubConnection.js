f4380d739fcb58a5e762ac6399dc0f9f649b63aa
Karl <git@karlneugebauer.com>
2021-10-13 17:28:28 +0200

    f all of this

--- classes/pubsub/TwitchPubSubConnection.js
+++ classes/pubsub/TwitchPubSubConnection.js
@@ -6,7 +6,7 @@ const WebSocket = require('ws')
 const Logger = require('../helper/Logger')
 
 const host = 'wss://pubsub-edge.twitch.tv'
-const heartbeatInterval = 1000 * 45 //ms between PING's
+const heartbeatInterval = 1000 * 15 //ms between PING's
 const reconnectInterval = 1000 * 10 //ms to wait before reconnect
 
 class TwitchPubSubConnection extends EventEmitter {
